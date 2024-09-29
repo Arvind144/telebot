1a612c3d8127a2a6ec0327fb16a63719c4f33234
Vikas Kumar <103965285+Arvind144@users.noreply.github.com>
2024-09-29 08:03:14 +0530

    Add files via upload

---
+++ readme.cpp
@@ -0,0 +1,7 @@
+step 1 :- python-telegram-bot: command not found
+
+step 2 :- pkg install git
+step 3 :- git clone your link 👍
+step 5 :- python broadcast.py
+
+for more information :- @introvert_danger
