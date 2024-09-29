01838f22664105e508a16ac663a715740ddab96c
Vikas Kumar <103965285+Arvind144@users.noreply.github.com>
2024-09-29 08:10:53 +0530

    Update readme.cpp

--- readme.cpp
+++ readme.cpp
@@ -1,8 +1,8 @@
 step 1 :-  pip install python-telegram-bot --upgrade
-
-
 step 2 :- pkg install git
-step 3 :- git clone your link 👍
-step 5 :- python broadcast.py
+step 3 :- git clone https://github.com/Arvind144/telebot.git
+step 4 :- cd telebot 
+step 5 :- ls
+step 6 :- python broadcast.py
 
 for more information :- @introvert_danger
